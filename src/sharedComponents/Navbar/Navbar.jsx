5787c68f871d3f8b57b57de2e35267e9b9e50723
mohdbinharun56 <arahaman461@gmail.com>
2025-03-02 14:50:28 +0600

    initialized react router and navbar

---
+++ src/sharedComponents/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    const navList = <>
+         <li><NavLink to='/'>Home</NavLink></li>
+         <li><NavLink to='/tourists'>Tourists Spot</NavLink></li>
+         <li><NavLink to='/tourist'>Add Tourists Spot</NavLink></li>
+         <li><NavLink to='tourists/list'>My List</NavLink></li>
+    </>
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                       {navList}
+                    </ul>
+                </div>
+                <NavLink className="btn btn-ghost text-xl" to='/'>TravelSphere</NavLink>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                   {
+                    navList
+                   }
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <NavLink className="btn">Login</NavLink>
+                <NavLink className="btn ml-3">Register</NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
